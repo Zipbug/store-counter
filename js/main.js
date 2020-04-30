7809002fddfacccd272894495c5edbfef26362fc
cdeskins <cody@cdeskins.com>
2020-04-30 12:04:41 -0600

    button styling on form

--- js/main.js
+++ js/main.js
@@ -47,12 +47,13 @@ $(function () {
   var JoinRoom = function(room, data){
     var clipboard = '<div class="clipboard"><svg class="room-data" data-room="'+room+'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="clipboard" x="0px" y="0px" width="780.974px" height="780.975px" viewBox="0 0 780.974 780.975" style="enable-background:new 0 0 780.974 780.975;" xml:space="preserve"><path d="M615.062,780.975h-449.16c-30.167,0-54.709-24.543-54.709-54.709V116.704c0-30.167,24.542-54.709,54.709-54.709h147.482     v42.362h153.205V61.995h148.473c30.171,0,54.719,24.542,54.72,54.708v609.563C669.782,756.432,645.235,780.975,615.062,780.975z      M165.902,81.79c-19.252,0-34.915,15.662-34.915,34.914v609.562c0,19.252,15.663,34.914,34.915,34.914h449.16     c19.258,0,34.925-15.662,34.925-34.914V116.704c-0.001-19.251-15.668-34.914-34.925-34.914H486.385v42.362H293.59V81.79H165.902z     "/><path d="M458.665,92.139H321.309V28.937h40.981v-1.135C362.291,12.472,374.763,0,390.094,0h0.359     c15.33,0,27.803,12.472,27.803,27.802v1.135h40.407L458.665,92.139L458.665,92.139z M341.104,72.344h97.766V48.732h-40.407     v-20.93c0-4.415-3.592-8.007-8.008-8.007h-0.359c-4.416,0-8.009,3.592-8.009,8.007v20.93h-40.981L341.104,72.344L341.104,72.344z"/></svg></div>';
       $('.main').html('<h4 class="center-card"><span id="total">'+data.total+'</span><sub>Current Occupancy</sub></h4>'+
-                        '<form id="p" >'+
-                        '<button type="submit">+1</button>'+
+                        '<div class=pm_container>' + '<form id="p" >'+
+                        '<button class="plus" type="submit">+1</button>'+
                         '</form>'+
                         '<form id="m" >'+
-                          '<button type="submit">-1</button>'+
+                          '<button class="minus" type="submit">-1</button>'+
                         '</form>'+
+                        '</div>'+ '<div class="clearfix"></div>' +
                         '<form id="max">'+
                         '<input id="max-oc" value="'+data.max+'"></input>'+
                         '<sub class="center-card">Max Occupancy</sub>'+
