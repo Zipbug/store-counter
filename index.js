bdc583361c703613f985e25846bcf1bee230c124
Eric Evans <eevans@pxp200.com>
2020-05-22 17:00:48 -0600

    added Terry's api calls

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){res.sendFile(__dirname + '/index.html');});
 app.use(express.static(__dirname + '/'));
-
-io.on('connection', function(socket){
-  socket.on('count', function(count_obj){
-    io.to(count_obj.room).emit('count', count_obj.total);
-    var socket_room = io.sockets.adapter.rooms[count_obj.room];
-    if(socket_room){
-      socket_room.roomNumber = count_obj.total;
-    }
-  });
-  socket.on('join', function(room_ojb){
-    var socket_room = io.sockets.adapter.rooms[room_ojb.room];
-
-    if(socket_room){
-      if(socket_room.pass == room_ojb.password){
-        socket.join(room_ojb.room);
-        var total = socket_room && socket_room.roomNumber ? socket_room.roomNumber : 0;
-        var max = socket_room && socket_room.maxOccupency ? socket_room.maxOccupency : 0;
-        io.to(room_ojb.room).emit('join', {"total": total, "max": max, "password": room_ojb.password});
-      }else{
-        socket.emit('exception', {errorMessage: 'Incorrect Password'});
-      }
-    }else{
-      socket.join(room_ojb.room);
-      socket_room = io.sockets.adapter.rooms[room_ojb.room];
-
-      if(socket_room){
-        socket_room.roomNumber = room_ojb.total ? room_ojb.total : 0;
-        socket_room.maxOccupency = room_ojb.max ? room_ojb.max :  0 ;
-        socket_room.pass = room_ojb.password;
-      }
-      io.to(room_ojb.room).emit('join', room_ojb);
-    }
-  });
-  socket.on('change_max', function(count_obj){
-    io.to(count_obj.room).emit('change_max', count_obj);
-    var socket_room = io.sockets.adapter.rooms[count_obj.room];
-    if(socket_room){
-      socket_room.roomNumber = count_obj.total;
-      socket_room.maxOccupency = count_obj.max;
-    }
-  });
-});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
--- js/main.js
+++ js/main.js
@@ -1,72 +1,199 @@
 $(function () {
-  $('#room').submit(function(){
-    var $url_rand = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    var $pass_rand = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    var $total = $('.total').val();
-    var $new_url =  window.location + "room/?i=" +$url_rand;
-    var $max = $('.max-oc').val();
+  /*
+    * Global Variables/ functions.
+    * Worker Helper functions.
+    * Form Submission.
+    * Page Render
+  */
+
+  /*----- Global Variables -----*/
+  var roomCheck = null;
+
+  /*----- Worker Helper functions -----*/
+  function callWorker(url, put, callback){
+    const Http = new XMLHttpRequest();
+    Http.open(put, url);
+
+    Http.send();
+    Http.onreadystatechange = (e) => {
+      if (Http.readyState === 4) {
+          var resp = JSON.parse(Http.response);
+          if(resp.error){
+            window.alert(resp.error);
+            return false;
+          }else{
+            callback(resp);
+          }
+      }
+    }
+  }
+
+  function processURL(){
+    var $location = new URL(window.location).searchParams.get("i");
+    if($location){
+      var $url = "https://65f34f5dz4.execute-api.us-east-1.amazonaws.com/dev/space/"  + $location;
+
+      callWorker($url, "GET", updateRoom);
+      roomCheck = setTimeout(function(){ processURL() }, 2000);
+    }
+  }
+
+  /*----- Form Submission -----*/
+  $('#room').submit(function(e){
+    e.preventDefault();
+
+    var $total = $('#starting_oc').val();
+    var $max = $('#max_oc').val();
     if(!$total){
       $total = 0;
     }
     if(!$max){
       $max = 0;
     }
-    document.cookie = "room_data=" + JSON.stringify({"room": $url_rand, "total": $total, "max": $max, "password": $pass_rand});
-    window.location.href = $new_url;
+    var $url= "https://65f34f5dz4.execute-api.us-east-1.amazonaws.com/dev/space/new/occupancy/current/"+$total+"/max/" + $max;
+
+
+    callWorker($url, "PUT", function(returnData){
+      var getUrl = window.location;
+      var $new_url =  getUrl .protocol + "//" + getUrl.host + "/" + "room/?i=" +returnData.space_id;
+      window.location.href = $new_url;
+    });
     return false;
   });
 
-  $('#addPassword').click(function(){
-    $('#password-group').removeClass("d-none");
-    $('#removePassword').removeClass("d-none");
-    $('#addPassword').addClass("d-none");
+  $('#m').submit(function(e){
+    e.preventDefault();
+
+    var $location = new URL(window.location).searchParams.get("i");
+    var $url = "https://65f34f5dz4.execute-api.us-east-1.amazonaws.com/dev/space/"+$location+"/decrement";
+
+
+    roomCheck = null;
+    callWorker($url,"PUT", updateRoom);
+    return false;
   });
 
-  $('#removePassword').click(function(){
-    $('#password-group').addClass("d-none");
-    $('#removePassword').addClass("d-none");
-    $('#addPassword').removeClass("d-none");
+  $('#p').submit(function(e){
+    e.preventDefault();
+
+    var $location = new URL(window.location).searchParams.get("i");
+    var $url = "https://65f34f5dz4.execute-api.us-east-1.amazonaws.com/dev/space/"+$location+"/increment";
+
+
+    roomCheck = null;
+    callWorker($url,"PUT", updateRoom);
+    return false;
   });
 
-  $('.nav-links a').click(function(){
-    $('#nav-drawer').removeClass("bmd-drawer-in");
+  $('#max-value-form').submit(function(){
+    var $max = parseInt($('#max-value').val());
+    var $location = new URL(window.location).searchParams.get("i");
+    var $url = "https://65f34f5dz4.execute-api.us-east-1.amazonaws.com/dev/space/"+$location+"/max/" + $max;
+
+   callWorker($url,"PUT", updateRoom);
+   $('#max-change').toggleClass('hidden');
+   return false;
   });
+
+
+  $('#contact').submit(function(){
+    var $obj = {
+     "call": 'contact',
+     "email ": $('#email').val(),
+     'message': $('#message').val(),
+   };
+   const Http = new XMLHttpRequest();
+   Http.open("POST", 'https://api.occupancyapp.com');
+   Http.setRequestHeader('Accept', 'application/json');
+   Http.setRequestHeader('Content-Type', 'application/json');
+   Http.send(JSON.stringify($obj));
+
+   Http.onreadystatechange = (e) => {
+
+     if (Http.readyState === 4) {
+         var resp = JSON.parse(Http.response);
+         if(resp.error){
+           window.alert(resp.error);
+           return false;
+         }else{
+            $('#contact').html(resp.message);
+         }
+     }
+   }
+
+    return false;
+  });
+
+  /*----- Page Render Functions -----*/
+
+  function updateRoom(data){
+    $('#current').text(parseInt(data.occupancy.current));
+    $('#max').text(parseInt(data.occupancy.maximum));
+    calulateGraph();
+    $('.room-count.hidden').removeClass('hidden');
+    clearTimeout(roomCheck);
+    roomCheck = setTimeout(function(){ processURL() }, 2000);
+  }
+
 
   // Calculate Percentage for graph
-  $(function(){
-    var current = 0
-    var maximum = 25
-    var percentage = (current*100/maximum);
-    if (percentage >= 100) {
-      overCapacity();
-    } else if (percentage >= 80 && percentage <= 99.9) {
-      console.log("yup, I was called");
-      yellowWarning();
-    } else {
-      normalize();
+  function calulateGraph(){
+    var current = $('#current').text();
+    var maximum = $('#max').text();
+    if(maximum !== 0){
+      var percentage = (current*100/maximum);
+        $('#progress-bar').css('width', percentage +"%");
+      if (percentage >= 100) {
+        overCapacity();
+      } else if (percentage >= 80 && percentage <= 99.9) {
+        yellowWarning();
+      } else {
+        normalize();
+      }
     }
-  });
+  }
 
   //almost at capacity
-  $(function yellowWarning(){
+  function yellowWarning(){
+    $('#progress-bar').removeClass("red");
+    $('#current').removeClass("red-text");
+    $('#nav-color').removeClass("red");
+    $('#over-capacity').addClass("d-none");
     $('#progress-bar').addClass("yellow");
-  });
+  }
 
   //over capacity
-  $(function overCapacity(){
+  function overCapacity(){
     $('#progress-bar').addClass("red");
     $('#current').addClass("red-text");
     $('#nav-color').addClass("red");
     $('#over-capacity').removeClass("d-none");
-  });
+  }
 
   //back to normal
-  $(function normalize(){
+  function normalize(){
     $('#progress-bar').removeClass("red").removeClass("yellow");
     $('#current').removeClass("red-text");
     $('#nav-color').removeClass("red");
     $('#over-capacity').addClass("d-none");
+  }
+
+  $('.nav-links a').click(function(){
+    $('#nav-drawer').removeClass("bmd-drawer-in");
+  });
+  $('.toggle_qr').click(function(){
+    QRCode(document.getElementById("qrcode"), window.location);
+    $('#qrcode').toggleClass('hidden');
   });
 
+  $('#copy').click(function(){
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(window.location).select();
+    document.execCommand("copy");
+    $temp.remove()
+    alert('copied to clipboard');
+  });
 
+  processURL();
 });
